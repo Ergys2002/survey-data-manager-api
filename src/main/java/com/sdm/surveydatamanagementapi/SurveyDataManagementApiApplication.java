e0291747c43b6394bf7002e06551fb4fbe9795ba
Ergys <ergysxhaollari02@gmail.com>
2025-02-13 19:42:56 +0100

    Initial Commit

---
+++ src/main/java/com/sdm/surveydatamanagementapi/SurveyDataManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.sdm.surveydatamanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyDataManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyDataManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdm/surveydatamanagementapi/SurveyDataManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdm.surveydatamanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyDataManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
