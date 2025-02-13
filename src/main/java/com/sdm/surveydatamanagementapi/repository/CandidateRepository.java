4137c0f6266dd751b28dcd7a7e854ae9c4f5df2a
Ergys <ergysxhaollari02@gmail.com>
2025-02-13 20:15:57 +0100

    Added repository layer
    Implemented using JpaRepository

---
+++ src/main/java/com/sdm/surveydatamanagementapi/repository/CandidateRepository.java
@@ -0,0 +1,13 @@
+package com.sdm.surveydatamanagementapi.repository;
+
+import com.example.surveydatamanager.entity.Candidate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CandidateRepository extends JpaRepository<Candidate, UUID> {
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
+}
---
+++ src/main/java/com/sdm/surveydatamanagementapi/repository/SurveyRepository.java
@@ -0,0 +1,11 @@
+package com.sdm.surveydatamanagementapi.repository;
+
+import com.example.surveydatamanager.entity.Survey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SurveyRepository extends JpaRepository<Survey, UUID> {
+}
